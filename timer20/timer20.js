33cb4a2679d0ad3ba574fab0330efa603bd85c05
elvina-s <rammnuschka@gmail.com>
2023-11-30 13:08:09 +0100

    first commit

---
+++ timer20/timer20.js
@@ -0,0 +1,68 @@
+function time() {
+    if (window.matchMedia("(max-width: 500px)").matches) {
+      const button = document.querySelector("#myButton");
+      button.addEventListener("click", calculateTime);
+  
+      function calculateTime() {
+        const countdown = document.querySelector("#countdown");
+        const timer = 20;
+        let amountTime = timer * 60;
+        let timerId = setInterval(function() {
+        let minutes = Math.floor(amountTime/60);
+        let seconds = amountTime%60;
+  
+        if(seconds < 10) {
+          seconds = "0" + seconds;
+      }
+  
+        countdown.textContent = `${minutes} : ${seconds}`;
+        amountTime--;
+  
+        if(amountTime < 0) {
+          stopTimer();
+          amountTime = 0;
+      }
+  
+      function stopTimer() {
+          clearInterval(timerId);
+          countdown.textContent = "Time is up!";
+      }
+  
+      }, 1000);
+  }
+  } 
+  
+    else {
+      const timer = 20;
+      let amountTime = timer * 60;
+  
+      function calculateTime() {
+        const countdown = document.querySelector("#countdown"); 
+        let minutes = Math.floor(amountTime/60);
+        let seconds = amountTime%60;
+      
+        if(seconds < 10) {
+          seconds = "0" + seconds;
+      }
+      
+      countdown.textContent = `${minutes} : ${seconds}`;
+      amountTime--;
+      
+        if(amountTime < 0) {
+          stopTimer();
+          amountTime = 0;
+      }
+      
+        function stopTimer() {
+          clearInterval(timerId);
+          countdown.textContent = "Time is up!";
+      }
+      }
+  
+        let timerId = setInterval(calculateTime, 1000);
+  }
+  }
+  
+  time();
+  
+  
